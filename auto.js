951b3b2c6f4c3d99fa7dd0b2cc93b968e6b53e0a
DAYFRE <121768693+DAYFRE@users.noreply.github.com>
2023-05-13 00:10:44 +0700

    Update auto.js

--- auto.js
+++ auto.js
@@ -14,7 +14,25 @@ const site = sites.find(s => currentUrl.includes(s.url));
 
 if (site) {
   fetch(site.scriptUrl)
-    .then(response => response.text())
-    .then(script => eval(script))
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Failed to retrieve ${site.scriptUrl} (${response.status} ${response.statusText})`);
+      }
+      const contentType = response.headers.get('content-type');
+      if (contentType.includes('application/json')) {
+        return response.json();
+      } else if (contentType.includes('application/javascript')) {
+        return response.text();
+      } else {
+        throw new Error(`Invalid content type ${contentType}`);
+      }
+    })
+    .then(script => {
+      if (typeof script === 'string') {
+        eval(script);
+      } else {
+        console.error('Invalid script format');
+      }
+    })
     .catch(error => console.error(error));
 }
