9f0e690779ae70055c3ee6680392dc0f1d0aa85b
DAYFRE <121768693+DAYFRE@users.noreply.github.com>
2023-05-13 00:14:10 +0700

    Update auto.js

--- auto.js
+++ auto.js
@@ -1,38 +1,9 @@
-const sites = [
-  {
-    url: "https://liveworksheets.com",
-    scriptUrl: "https://raw.githubusercontent.com/DAYFRE/Auto/main/liveworksheets.js"
-  },
-  {
-    url: "https://docs.google.com",
-    scriptUrl: "https://raw.githubusercontent.com/DAYFRE/Auto/main/GoogleFrom.js"
-  }
-];
-
-const currentUrl = window.location.href;
-const site = sites.find(s => currentUrl.includes(s.url));
-
-if (site) {
-  fetch(site.scriptUrl)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`Failed to retrieve ${site.scriptUrl} (${response.status} ${response.statusText})`);
-      }
-      const contentType = response.headers.get('content-type');
-      if (contentType.includes('application/json')) {
-        return response.json();
-      } else if (contentType.includes('application/javascript')) {
-        return response.text();
-      } else {
-        throw new Error(`Invalid content type ${contentType}`);
-      }
-    })
-    .then(script => {
-      if (typeof script === 'string') {
-        eval(script);
-      } else {
-        console.error('Invalid script format');
-      }
-    })
-    .catch(error => console.error(error));
-}
+if (window.location.href.includes("https://liveworksheets.com")) {
+  fetch("https://raw.githubusercontent.com/DAYFRE/Auto/main/liveworksheets.js")
+  .then(a => a.text().then(b => eval(b)))
+  .catch(error => console.error(error));
+} else if (window.location.href.includes("https://liveworksheets.com")) {
+  fetch("https://raw.githubusercontent.com/DAYFRE/Auto/main/GoogleFrom.js")
+  .then(a => a.text().then(b => eval(b)))
+  .catch(error => console.error(error));
+} 
