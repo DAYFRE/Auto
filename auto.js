59c9d6d980f5856961807cbbd2a31141c6bea1b1
DAYFRE <121768693+DAYFRE@users.noreply.github.com>
2023-05-13 01:25:54 +0700

    Update auto.js

--- auto.js
+++ auto.js
@@ -1,4 +1,4 @@
-else if (window.location.href.includes("liveworksheets.com")) {
+if (window.location.href.includes("liveworksheets.com")) {
   fetch("https://raw.githubusercontent.com/DAYFRE/Auto/main/liveworksheets.js")
   .then(a => a.text().then(b => eval(b)))
   .catch(error => console.error(error));
