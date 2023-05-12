4bcde733f6ca41af165b58bb3fddfc55f6e6adc4
DAYFRE <121768693+DAYFRE@users.noreply.github.com>
2023-05-12 23:19:56 +0700

    Update auto.js

--- auto.js
+++ auto.js
@@ -1,15 +1,14 @@
-const urlMap = new Map([
-  ["https://liveworksheets.com", "https://raw.githubusercontent.com/DAYFRE/Auto/main/liveworksheets.js"],
-  ["https://docs.google.com", "https://raw.githubusercontent.com/DAYFRE/Auto/main/GoogleFrom.js"]
-]);
-
-const currentUrl = window.location.href;
-const matchingUrl = Array.from(urlMap.keys()).find(url => currentUrl.includes(url));
-
-if (matchingUrl) {
-  const scriptUrl = urlMap.get(matchingUrl);
-  fetch(scriptUrl)
-    .then(a => a.text())
-    .then(b => eval(b))
-    .catch(error => console.error(error));
+switch (true) {
+  case window.location.href.includes("https://liveworksheets.com"):
+    fetch("https://raw.githubusercontent.com/DAYFRE/Auto/main/liveworksheets.js")
+      .then(a => a.text().then(b => eval(b)))
+      .catch(error => console.error(error));
+    break;
+  case window.location.href.includes("https://docs.google.com"):
+    fetch("https://raw.githubusercontent.com/DAYFRE/Auto/main/GoogleFrom.js")
+      .then(a => a.text().then(b => eval(b)))
+      .catch(error => console.error(error));
+    break;
+  default:
+    console.log("No matching case found.");
 }
