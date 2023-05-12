970739cc2a0818afc2176d9b49bb0ebfa947c2ee
DAYFRE <121768693+DAYFRE@users.noreply.github.com>
2023-05-12 23:20:54 +0700

    Update auto.js

--- auto.js
+++ auto.js
@@ -1,14 +1,9 @@
-switch (true) {
-  case window.location.href.includes("https://liveworksheets.com"):
-    fetch("https://raw.githubusercontent.com/DAYFRE/Auto/main/liveworksheets.js")
-      .then(a => a.text().then(b => eval(b)))
-      .catch(error => console.error(error));
-    break;
-  case window.location.href.includes("https://docs.google.com"):
-    fetch("https://raw.githubusercontent.com/DAYFRE/Auto/main/GoogleFrom.js")
-      .then(a => a.text().then(b => eval(b)))
-      .catch(error => console.error(error));
-    break;
-  default:
-    console.log("No matching case found.");
-}
+if (window.location.href.includes("https://liveworksheets.com")) {
+  fetch("https://raw.githubusercontent.com/DAYFRE/Auto/main/liveworksheets.js")
+  .then(a => a.text().then(b => eval(b)))
+  .catch(error => console.error(error));
+} else if (window.location.href.includes("https://docs.google.com")) {
+  fetch("https://raw.githubusercontent.com/DAYFRE/Auto/main/GoogleFrom.js")
+  .then(a => a.text().then(b => eval(b)))
+  .catch(error => console.error(error));
+} 
