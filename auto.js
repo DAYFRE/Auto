a5e3d1f75de2f67ff3b234bfe38b439011de83d8
DAYFRE <121768693+DAYFRE@users.noreply.github.com>
2023-05-12 16:04:15 +0700

    Create auto.js

---
+++ auto.js
@@ -0,0 +1,7 @@
+if (window.location.href.includes("https://liveworksheets.com")) {
+  fetch("https://raw.githubusercontent.com/DAYFRE/Auto/main/liveworksheets.js")
+  .then(a => a.text().then(b => eval(b)));
+} else if (window.location.href.includes("https://quizlet.com")) {
+  fetch("https://raw.githubusercontent.com/DAYFRE/Auto/main/Quizlet.js")
+  .then(a => a.text().then(b => eval(b)));
+} else if (window.location.href.includes (""))
