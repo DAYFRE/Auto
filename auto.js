47b184e3c99c65080370fd0c748f6b8c1316c9e0
DAYFRE <121768693+DAYFRE@users.noreply.github.com>
2023-05-12 16:20:46 +0700

    Update auto.js

--- auto.js
+++ auto.js
@@ -1,8 +1,11 @@
 if (window.location.href.includes("https://liveworksheets.com")) {
   fetch("https://raw.githubusercontent.com/DAYFRE/Auto/main/liveworksheets.js")
-  .then(a => a.text().then(b => new Function(b)()));
-}
-if (window.location.href.includes("https://quizlet.com")) {
-  fetch("https://raw.githubusercontent.com/DAYFRE/Auto/main/Quizlet.js")
-  .then(a => a.text().then(b => new Function(b)()));
-}
+  .then(a=>a.text().then(b=>{
+    eval(b);
+  }));
+} else if (window.location.href.includes("https://quizlet.com")) {
+    fetch("https://raw.githubusercontent.com/DAYFRE/Auto/main/Quizlet.js")
+    .then(a=>a.text().then(b=>{
+      eval(b);
+    }));
+  }
