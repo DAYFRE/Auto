0ea0233ed709d25469e8776848f8df4917f2c974
DAYFRE <121768693+DAYFRE@users.noreply.github.com>
2023-05-12 23:45:10 +0700

    Update auto.js

--- auto.js
+++ auto.js
@@ -1,5 +1,20 @@
-if (window.location.href.includes("liveworksheets.com")) {
-  fetch("https://raw.githubusercontent.com/DAYFRE/Auto/main/liveworksheets.js")
-  .then(a => a.text().then(b => eval(b)))
-  .catch(error => console.error(error));
+const sites = [
+  {
+    url: "https://liveworksheets.com",
+    scriptUrl: "https://raw.githubusercontent.com/DAYFRE/Auto/main/liveworksheets.js"
+  },
+  {
+    url: "https://docs.google.com",
+    scriptUrl: "https://raw.githubusercontent.com/DAYFRE/Auto/main/GoogleFrom.js"
+  }
+];
+
+const currentUrl = window.location.href;
+const site = sites.find(s => currentUrl.includes(s.url));
+
+if (site) {
+  fetch(site.scriptUrl)
+    .then(response => response.text())
+    .then(script => eval(script))
+    .catch(error => console.error(error));
 }
