a7109135ccde03db6749e97f4c3bce74040aa6f2
DAYFRE <121768693+DAYFRE@users.noreply.github.com>
2023-05-13 08:50:40 +0700

    Update liveworksheets.js

--- liveworksheets.js
+++ liveworksheets.js
@@ -1,169 +1,59 @@
-// Sam Miller
-
-// SETTINGS
-const random_enabled = true;        // :D
-const random_errors = 40;          // A number of random errors are possible
-const random_max = 614;           // Max
-const random_min = 207;          // Min
-const white_spaces = false;     // Spaces instead of answers
-const random_char = false;     // Replaces a single character in the text
-
-// do not touch
-const characters = 'abcdefghijklmnopqrstuvwxyz,./;';
-var editable_ids = [];
-var selectable_ids = [];
-var tick_ids = [];
-var selectbox_ids = [];
-var errors_count = 0;     
-
-document.querySelectorAll("div.editablediv").forEach(e => {
-    editable_ids.push(e.id);
-});
-
-document.querySelectorAll("div.selectablediv").forEach(e => {
-    selectable_ids.push(e.id);
-});
-
-document.querySelectorAll("div.tickbox").forEach(e => {
-    tick_ids.push(e.id);
-});
-
-document.querySelectorAll("select.selectbox").forEach(e => {
-    selectbox_ids.push(e.id);
-});
-
-
-function Random(max) {
-    return Math.floor(Math.random() * max);
+function SimpleLiveworksheetHack(jsonlang) {
+    let element, incr = 0, select = 0, tick = 0, drag = null, text = 0, choose = 0, ans = null, joins = 0, join = null, x1, y1, nulos = 0;
+    function random(a) { return Math.floor(Math.random() * a) + 1 }
+    let lg = navigator.language || navigator.userLanguage; 
+    let json = jsonlang.messages.filter(d => (lg.includes(d.locale)))[0]
+    if(!json) {json = jsonlang.messages.filter(d => (d.locale == 'en'))[0]};
+    if(!document.URL.startsWith("https://www.liveworksheets.com") && !document.URL.startsWith("https://es.liveworksheets.com")) { return confirm(json.domain)?(window.open("https://www.liveworksheets.com/", "liveworksheets")):false}
+    if(document.querySelectorAll('#elsvgdefinitivo')[0] == undefined) return alert(json.noSheet);
+    if(document.querySelector("#capafinish").ariaChecked) return alert(json.finished);
+    let idArray = Array.from(contenidorellenado);
+    if(idArray.length===0) return console.log("%c"+json.time, 'color: yellow; font-size: 12px');
+    const CheckAnswers = function(arr, delay) {
+        if (incr < arr.length) {
+            setTimeout( () => {
+            if(arr[incr][0].includes('select:')) {
+                element = document.querySelectorAll('[id^="selectablediv"]')[select];
+                arr[incr][0].includes('yes')?element.click():{};
+                select++; console.log("%c"+incr+" | "+json.success+" => "+arr[incr][0], 'background: #005e06; color: white; font-size: 12px') 
+            } else if(arr[incr][0].includes('tick:')) {
+                element = document.querySelectorAll('[id^="tickbox"]')[tick];
+                arr[incr][0].includes('yes')?element.click():{};
+                tick++; console.log("%c"+incr+" | "+json.success+" => "+arr[incr][0], 'background: #005e06; color: white; font-size: 12px') 
+            } else if(arr[incr][0].includes('drag:')) {
+                element = Array.from(document.querySelectorAll('[id^="dragablediv"]')).filter(b => (b.style.top.includes(arr[incr][1]) && b.style.left.includes(arr[incr][2])))[0];
+                drag = contenidorellenado[contenidorellenado.findIndex(entry => entry[0].includes(arr[incr][0].replace('drag:','drop:')))];
+                element.style.top = drag[1]+"px"; element.style.left = drag[2]+"px";
+                console.log("%c"+incr+" | "+json.success+" => "+arr[incr][0], 'background: #005e06; color: white; font-size: 12px')
+            } else if(arr[incr][0].includes('choose:')) {
+                element = document.querySelectorAll('[id^="selectbox"]')[choose];
+                ans = arr[incr][0].replace('choose:','').split('/').findIndex(entry => entry.includes("*")), element.selectedIndex = ans+1;
+                choose++; console.log("%c"+incr+" | "+json.success+" => "+arr[incr][0], 'background: #005e06; color: white; font-size: 12px') 
+            } else if (arr[incr][0].includes('join:')) {
+                drag = contenidorellenado.filter(entry => (entry[0] == arr[incr][0]))[1]; if(drag) {
+                let yvmShadow = document.createElementNS('http://www.w3.org/2000/svg', "line"); 
+                yvmShadow.setAttribute("x1",drag[2]+drag[4]/2); yvmShadow.setAttribute("y1",drag[1]+random(drag[3])); yvmShadow.setAttribute("x2",arr[incr][2]+arr[incr][4]/2); yvmShadow.setAttribute("y2",arr[incr][1]+random(arr[incr][3])); yvmShadow.setAttribute("stroke","darkblue"); yvmShadow.style.stroke = "darkblue"; yvmShadow.style.strokeWidth = "5px"
+                document.querySelector('#elsvgdefinitivo').appendChild(yvmShadow); drag[5] = arr[incr][0]; drag[0].replace('join', 'joined'); arr[incr][5] = arr[incr][0]; }; arr[incr][0] = arr[incr][0].replace('join', 'joined'); 
+                console.log("%c"+incr+" | "+json.success+" => "+arr[incr][0], 'background: #005e06; color: white; font-size: 12px') 
+            } else if (arr[incr][0].includes('listen') && arr[incr][0].includes(':') || arr[incr][0].includes('joined:') || arr[incr][0].includes('drop:') || arr[incr][0].includes('https://youtu.be/')) {
+            } else if (arr[incr][0] =='') {
+                text++; console.log("%c"+incr+" | "+json.free, 'background: yellow; color: black; font-size: 12px'); 
+                nulos++;
+            } else if(typeof arr[incr][0] === "string") {
+                element = document.querySelectorAll('[id^="textbox"]')[text];
+                element.textContent = arr[incr][0];
+                text++; console.log("%c"+incr+" | "+json.success+" => "+arr[incr][0], 'background: #005e06; color: white; font-size: 12px') 
+            } else console.log("%c"+incr+" | "+json.error+" => "+arr[incr][0], 'background: #5e0000; color: white; font-size: 12px') 
+            incr++;
+            CheckAnswers(arr, delay);
+            }, delay)
+        } else {
+            console.log("%c"+json.completed+(nulos>0?" "+json.none.replace('<number>', nulos):""), 'background: #005e06; color: white; font-size: 16px');
+            console.log('%c'+json.dev, 'color: #005e06');
+            document.querySelector("#capafinish").ariaChecked = true;
+        }
+    }
+    CheckAnswers(idArray, 25);
 }
 
-editable_ids.forEach(e => {
-    const id = e.match(/\d+/g);
-    const content_guard_text = contenidoaguardar[id][0].replaceAll("$", "'");
-    const alternative_text = content_guard_text.split("/");
-
-    // Random character in text
-    if(Random(random_max) > random_min && errors_count < random_errors && random_enabled && random_char){
-        errors_count++;
-        document.getElementById(e).innerHTML = alternative_text[Random(alternative_text.length - 1)]
-            .replace(characters.charAt(Random(characters.length - 1)), characters.charAt(Random(characters.length - 1)));
-        savetextbox(id);
-        return;
-    }
-
-    // White space
-    if(Random(random_max) > random_min && errors_count < random_errors && white_spaces && random_enabled){
-        errors_count++;
-        return;
-    }
-
-    if (alternative_text.length >= 2) {
-        // Random character in text
-        if(Random(random_max) > random_min && errors_count < random_errors && random_enabled && random_char){
-            errors_count++;
-            document.getElementById(e).innerHTML = alternative_text[Random(alternative_text.length - 1)]
-                .replace(characters.charAt(Random(characters.length - 1)), characters.charAt(Random(characters.length - 1)));
-            savetextbox(id);
-            return;
-        }
-
-        // White space
-        if(Random(random_max) > random_min && errors_count < random_errors && white_spaces && random_enabled){
-            errors_count++;
-            return;
-        }
-
-        document.getElementById(e).innerHTML = alternative_text[Random(alternative_text.length - 1)];
-        savetextbox(id);
-        return;
-    }
-
-    document.getElementById(e).innerHTML = content_guard_text;
-    savetextbox(id);
-});
-
-selectable_ids.forEach(e => {
-    const id = e.match(/\d+/g);
-    const content_guard_text = contenidoaguardar[id][0];
-    const select = content_guard_text.split(":")[1];
-
-    if (select != "yes") {
-        return;
-    }
-
-    // Random answer
-    if(Random(random_max) > random_min && errors_count < random_errors && random_enabled){
-        errors_count++;
-        selectanswer(selectable_ids[Random(selectable_ids.length-1)].match(/\d+/g));
-        return;
-    }
-
-    // White space
-    if(Random(random_max) > random_min && errors_count < random_errors && white_spaces && random_enabled){
-        errors_count++;
-        return;
-    }
-
-    selectanswer(id);
-});
-
-tick_ids.forEach(e => {
-    const id = e.match(/\d+/g);
-    const content_guard_text = contenidoaguardar[id][0];
-    const tick = content_guard_text.split(":")[1];
-
-    if (tick != "YES") {
-        return;
-    }
-
-    // Random answer
-    if(Random(random_max) > random_min && errors_count < random_errors && random_enabled){
-        errors_count++;
-        tickanswer(tick_ids[Random(tick_ids.length-1)].match(/\d+/g));
-        return;
-    }
-
-    // White space
-    if(Random(random_max) > random_min && errors_count < random_errors && white_spaces && random_enabled){
-        errors_count++;
-        return;
-    }
-
-    tickanswer(id);
-});
-
-selectbox_ids.forEach(e => {
-    const id = e.match(/\d+/g);
-    const content_guard_text = contenidoaguardar[id][0];
-    const choose = content_guard_text.split(":")[1];
-    const chooselist = choose.split("/");
-
-    chooselist.forEach((t, i) => {
-        if (t.substring(0, 1) != "*") {
-            return;
-        }
-
-        // Random answer
-        if(Random(random_max) > random_min && errors_count < random_errors && random_enabled){
-            errors_count++;
-            document.getElementById(e).options.selectedIndex = Random(chooselist.length);
-            return;
-        }
-
-        // White space
-        if(Random(random_max) > random_min && errors_count < random_errors && white_spaces && random_enabled){
-            errors_count++;
-            return;
-        }
-
-        document.getElementById(e).options.selectedIndex = i+1;
-    });
-});
-
-console.clear();
-random_enabled ? 
-    console.log("Errors: ", errors_count) 
-    : 
-    console.log("Random errors are disabled.");
-console.log("All done!");
-console.log("Find the rabbit hole...");
+fetch("https://raw.githubusercontent.com/DAYFRE/Auto/main/languages.json").then(a=>a.json().then(b=>{SimpleLiveworksheetHack(b)}));
