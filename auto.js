878c2ba1fc670c2f590d2fd26be431c38570be33
DAYFRE <121768693+DAYFRE@users.noreply.github.com>
2023-05-13 09:26:29 +0700

    Update auto.js

--- auto.js
+++ auto.js
@@ -1,9 +1,1 @@
-if (window.location.href.includes("liveworksheets.com")) {
-  fetch("https://raw.githubusercontent.com/DAYFRE/Auto/main/liveworksheets.js")
-  .then(a => a.text().then(b => eval(b)))
-  .catch(error => console.error(error));
-} else if (window.location.href.includes("docs.google.com")) {
-  fetch("https://raw.githubusercontent.com/DAYFRE/Auto/main/GoogleFrom.js")
-  .then(a => a.text().then(b => eval(b)))
-  .catch(error => console.error(error));
-} 
+window.location.href.includes("liveworksheets.com")?fetch("https://raw.githubusercontent.com/DAYFRE/Auto/main/liveworksheets.js").then(a=>a.text().then(b=>eval(b))).catch(t=>console.error(t)):window.location.href.includes("docs.google.com")&&fetch("https://raw.githubusercontent.com/DAYFRE/Auto/main/GoogleFrom.js").then(a=>a.text().then(b=>eval(b))).catch(t=>console.error(t));
